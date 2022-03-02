ccc4f0072863e0554e252082e78ace835fd90be6
nsagark <90008930+nsagark@users.noreply.github.com>
2022-03-02 00:32:47 -0500

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,14 +1,57 @@
 terraform {
-  required_providers {
-    nirmata = {
-      source  = "nirmata/nirmata"
-      version = "1.1.7-rc7"
+    required_providers {
+        aws = {
+            source  = "hashicorp/aws"
+            version = "~> 3.0"
+        }
+        nirmata = {
+            source = "nirmata/nirmata"
+            version = "1.1.7-rc7"
+        }
     }
-  }
+    backend "remote" {
+      organization = "Nirmata"
+      workspaces {
+        name = "gh-actions-demo"
+      }
+    }
+}
+
+
+variable "project_name" {
+    type = string
+    default = "eks-demo"
+}
+
+provider "aws" {
+    region     = var.region
+    access_key = var.access_key
+    secret_key = var.secret_key
 }
 
 provider "nirmata" {
 
-  url   = var.url
+  # Nirmata address.
+  url = "https://nirmata.io"
+
+  // Nirmata API Key. Also configurable using the environment variable NIRMATA_TOKEN.
   token = var.nirmata_token
+
 }
+
+variable "nirmata_token" {
+  type = string
+}
+
+variable "access_key" {
+  type = string
+}
+
+variable "secret_key" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
+
